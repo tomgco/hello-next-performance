74ad4d0e9bbfc1224e5e7c96931dcfd8236c9438
Tom Gallacher <tomgallacher23@gmail.com>
2019-01-18 15:21:01 +0000

    Adding perf issues

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
-const Index = () => (
+const crypto = require('crypto');
+const util = require('util');
+
+const Index = (props) => (
   <div>
-    <p>Hello Next.js</p>
+    <p>Hello Next.js
+    {props.time.map(date => (
+      <b> {date.toString()} </b>
+    ))}
+    with generated hash {props.hash}
+    </p>
   </div>
 )
+
+const pbkdf2 = util.promisify(crypto.pbkdf2);
+
+Index.getInitialProps = async function() {
+  const data = [ Date.now() ]
+  const secret = await pbkdf2('secret', 'salt', 100000, 64, 'sha512')
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    time: data,
+    hash: secret.toString('hex')
+  }
+}
 
 export default Index
